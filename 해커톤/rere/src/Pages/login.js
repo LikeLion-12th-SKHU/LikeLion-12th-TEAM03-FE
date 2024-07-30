90e49d2b068633e4ad785dac940a96fd6c2cb429
Yun, HyunSeung <hyun136000@gamil.com>
2024-07-30 12:55:46 +0900

    chore: 로그인 테스트

--- 해커톤/rere/src/Pages/login.js
+++ 해커톤/rere/src/Pages/login.js
@@ -42,42 +42,53 @@ function Login() {
         }
       } else {
         setError(result.message || "Login failed.");
+
+        const data = await response.json();
+        setIsLoading(false);
+
+        if (response.ok) {
+          // 로그인 성공 시 토큰을 로컬 스토리지에 저장
+          localStorage.setItem("token", data.token);
+          navigate("/psytest");
+        } else {
+          setError(data.message);
+        }
       }
     } catch (error) {
       setIsLoading(false);
       setError("An error occurred. Please try again.");
       console.error("Login error:", error);
     }
-  };
 
-  return (
-    <div className="login-container">
-      <img src="/logo.png" alt="Logo" className="login-image" />
-      <form className="login-form" onSubmit={handleLogin}>
-        <h2>로그인</h2>
-        {error && <p className="error-message">{error}</p>}
-        <input
-          type="text"
-          value={loginId}
-          onChange={(e) => setLoginId(e.target.value)}
-          placeholder="Id"
-          required
-          className="login-input"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-          className="login-input"
-        />
-        <button type="submit" className="login-button">
-          {isLoading ? "Logging in..." : "Login"}
-        </button>
-      </form>
-    </div>
-  );
+    return (
+      <div className="login-container">
+        <img src="/logo.png" alt="Logo" className="login-image" />
+        <form className="login-form" onSubmit={handleLogin}>
+          <h2>로그인</h2>
+          {error && <p className="error-message">{error}</p>}
+          <input
+            type="text"
+            value={loginId}
+            onChange={(e) => setLoginId(e.target.value)}
+            placeholder="Id"
+            required
+            className="login-input"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+            className="login-input"
+          />
+          <button type="submit" className="login-button">
+            {isLoading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+      </div>
+    );
+  };
 }
 
 export default Login;
