4bdce813392dcf8a8b9d73214bdd5fe37f99cd44
Sungpie <sungsooben@naver.com>
2024-08-04 02:04:53 +0900

    chore: 메인 컨테이너 부분 디자인 수정

--- 해커톤/rere/src/Pages/userWrite.js
+++ 해커톤/rere/src/Pages/userWrite.js
@@ -162,10 +162,9 @@ function UserWrite() {
 
       if (response.ok) {
         alert("글이 성공적으로 작성되었습니다.");
-        navigate(-1);
       } else {
         console.error("Error response:", responseText);
-        alert(`모든 값을 채워주세요!`);
+        alert(`글 작성에 실패했습니다: ${responseText}`);
       }
     } catch (error) {
       console.error("Error:", error);
